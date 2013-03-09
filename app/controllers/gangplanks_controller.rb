6bf2db9ebc7b67201178332388938caa3ef8d6d1
Roy van de Water <roy@integrumtech.com>
2013-03-09 09:46:55 -0700

    Remove vestigial files from before move to backbone

--- app/controllers/gangplanks_controller.rb
+++ app/controllers/gangplanks_controller.rb
@@ -1,15 +1,4 @@
 class GangplanksController < ApplicationController
   def show
   end
-
-  def update
-    @gangplank = Gangplank.first
-    @gangplank.update_attributes status_params
-    redirect_to root_path
-  end
-
-private
-  def status_params
-    params.require(:gangplank).permit :open_until
-  end
 end
